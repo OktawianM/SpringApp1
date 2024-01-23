3c97dbe63b3697489346d1dca7025c62d7fd54cc
OktawianM <oktawianmankowski@gmail.com>
2024-01-23 12:35:51 +0100

    Studet's class created

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.student.Student;
+
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public String hello(){
-		return "hello World";
+	public List<Student> hello(){
+		return List.of(
+						new Student(
+							1L,
+							"Katarzyna",
+							33
+						)
+		);
 	}
 }
